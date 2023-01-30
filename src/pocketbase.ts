fcbb25a7088bb95fe06b2f9ca3ffa57d622a6279
Brady Underwood <underwood.brady@gmail.com>
2023-01-30 03:05:57 -0700

    removed pocketbase from core project

--- src/pocketbase.ts
+++ src/pocketbase.ts
@@ -2,7 +2,7 @@ import PocketBase from 'pocketbase';
 
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase("http://127.0.0.1:8090/");
+export const pb = new PocketBase("https://patient-tree-979.fly.dev");
 
 export const currentUser = writable(pb.authStore.model);
 
