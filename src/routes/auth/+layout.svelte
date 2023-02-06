255796c4ad28f51eefd381ed98d01895798644bc
Brady Underwood <underwood.brady@gmail.com>
2023-02-06 16:06:20 -0700

    maybe

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -1,24 +1,34 @@
 <script>
 	import '../../app.css';
-    import { webVitals } from '$lib/vitals';
-
+	import { webVitals } from '$lib/vitals';
+	// @ts-ignore
+	import { browser } from '$app/env';
+	import { page } from '$app/stores';
+	let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+	$: if (browser && analyticsId) {
+		webVitals({
+			path: $page.url.pathname,
+			params: $page.params,
+			analyticsId
+		});
+	}
 </script>
 
-<style>
-    main{
-    background: url(/backdrop.png) no-repeat center center fixed;
-    background-size: cover;
-    background-attachment: fixed;
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    }
-</style>
-
 <main>
-    <div class="flex justify-center items-center bg-black/10 h-screen flex-col">
-        <slot />
-    </div>
+	<div class="flex justify-center items-center bg-black/10 h-screen flex-col">
+		<slot />
+	</div>
 </main>
+
+<style>
+	main {
+		background: url(/backdrop.png) no-repeat center center fixed;
+		background-size: cover;
+		background-attachment: fixed;
+		position: absolute;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+	}
+</style>
